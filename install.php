85f3b41efbabcfc3607c017e5cb0814b9d7376c0
Sascha Weidner <support@sioweb.de>
2015-11-06 07:36:31 +0100

    fixed install/uninstall

--- install.php
+++ install.php
@@ -1,13 +1,16 @@
 <?php
 
+slice_ui::emptyClipboard(1);
+
 $fields = [
   'active'=>'char(1) NOT NULL default "1"',
   'online_from' => 'int(11) UNSIGNED NOT NULL DEFAULT "0"',
   'online_to' => 'int(11) UNSIGNED NOT NULL DEFAULT "0"',
 ];
+  
+$cols = rex_sql::showColumns(rex::getTablePrefix().'article_slice');
 foreach($fields as $fieldname => $fieldtype) {
   $found = false;
-  $cols = rex_sql::showColumns(rex::getTablePrefix().'article_slice');
   foreach($cols as $field) {
     if($field['name'] === $fieldname) {
       $found = true;
--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,25 @@
 <?php
 
-$found = false;
-$cols = rex_sql::showColumns(rex::getTablePrefix().'article_slice');
-foreach($cols as $field) {
-  if($field['name'] === 'active') {
-    $found = true;
-    break;
-  }
-}
+slice_ui::emptyClipboard(1);
 
-if($found) {
-  $sql = rex_sql::factory();
-  $sql->setQuery("ALTER TABLE `".rex::getTablePrefix().'article_slice'."` DROP active",array());
+$fields = [
+  'active',
+  'online_from',
+  'online_to',
+];
+
+$cols = rex_sql::showColumns(rex::getTablePrefix().'article_slice');
+foreach($fields as $fieldname) {
+  $found = false;
+  foreach($cols as $field) {
+    if($field['name'] === $fieldname) {
+      $found = true;
+      break;
+    }
+  }
+
+  if($found) {
+    $sql = rex_sql::factory();
+    $sql->setQuery("ALTER TABLE `".rex::getTablePrefix().'article_slice'."` DROP ".$fieldname,array());
+  }
 }
