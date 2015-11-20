00cd1f6c7627dd96f6747d56fbff6d89dd7ac928
Sascha Weidner <support@sioweb.de>
2015-11-20 08:41:31 +0100

    bug fix

--- plugins/slice_footer/boot.php
+++ plugins/slice_footer/boot.php
@@ -5,18 +5,20 @@ if(rex::isBackend()) {
   rex_view::addJsFile($this->getAssetsUrl('slice_footer.js'));
 }
 
-if(rex::isBackend() && is_object(rex::getUser()))
-  rex_perm::register('slice_ui[editall]', null, rex_perm::OPTIONS);
+if(is_object(rex::getUser())) {
+  if(rex::isBackend() && is_object(rex::getUser()))
+    rex_perm::register('slice_ui[editall]', null, rex_perm::OPTIONS);
 
-if(rex::getUser()->hasPerm('slice_ui[editall]')) {
-  rex_extension::register('ADD_AFTER_SLICE','slice_footer::editAll');
-  // slice_footer::extendSliceButtons();
+  if(rex::getUser()->hasPerm('slice_ui[editall]')) {
+    rex_extension::register('ADD_AFTER_SLICE','slice_footer::editAll');
+    // slice_footer::extendSliceButtons();
 
-  if(strpos(rex_request('page'),'content/editall') !== false)
-    slice_footer::showForm();
-  if(strpos(rex_request('page'),'content/delete') !== false)
-    slice_footer::deleteSlices();
+    if(strpos(rex_request('page'),'content/editall') !== false)
+      slice_footer::showForm();
+    if(strpos(rex_request('page'),'content/delete') !== false)
+      slice_footer::deleteSlices();
 
-  rex_extension::register('STRUCTURE_CONTENT_BEFORE_SLICES','slice_footer::addFooterForm');
-  rex_extension::register('STRUCTURE_CONTENT_AFTER_SLICES','slice_footer::addFooterForm');
+    rex_extension::register('STRUCTURE_CONTENT_BEFORE_SLICES','slice_footer::addFooterForm');
+    rex_extension::register('STRUCTURE_CONTENT_AFTER_SLICES','slice_footer::addFooterForm');
+  }
 }
