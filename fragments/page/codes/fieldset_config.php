284c53b9f59f5527d8fc8df33944f272de988b99
Sascha Weidner <support@sioweb.de>
2015-12-02 15:01:03 +0100

    how to use block config

---
+++ fragments/page/codes/fieldset_config.php
@@ -0,0 +1,10 @@
+<fieldset class="ui_json_blocks" data-min="2" data-max="4">
+  <legend>JSON-Values</legend>
+
+  <?php $index = 0;foreach('REX_JSON_VALUE[5]' as $key => $data) {?>
+  <div class="block" data-json="1">
+    <!-- html goes here -->
+  </div>
+  <?php $index++;}?>
+  
+</fieldset>
--- fragments/page/help.php
+++ fragments/page/help.php
@@ -1,2 +1,10 @@
 <p>FDL Copy fügt jedem Slice bis zu drei neue Icons hinzu. Dabei wird berücksichtigt, welche Aktion von einem Benutzer gewählt wurde. Wählt der Benutzer die Funktion Ausschneiden, kann er den Slice nicht nach sich selbst einfügen.</p>
 <p>@todo :)</p>
+<h3>Entwickler</h3>
+<p>Folgende Einstellungen können Entwickler in den Slices nutzen:</p>
+<h4>JSON-Block</h4>
+<p>Bekommt der übergeordnete Container - meist das Fieldset - die Klasse <b>class="ui_json_blocks"</b> können die Attribute data-min="[0-9]" und data-max="[0-9]" verwendet werden, um den Benutzer zu zwingen eine genaue Anzahl Blocks zu erstellen. Damit können zum Beispiel Galerien und Grids realisiert werden.</p>
+<p>Mit folgendem Beispiel müssen mindestens 2 Blöcke definiert werden. Es dürfen aber maximal 4 definiert werden.</p>
+<hr>
+<code><?php highlight_file('codes/fieldset_config.php');?></code>
+<hr>
