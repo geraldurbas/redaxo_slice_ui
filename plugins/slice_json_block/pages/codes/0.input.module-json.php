725d4fd4619225777d95e6a98ede6e94adf3b859
Sascha Weidner <support@sioweb.de>
2015-12-02 11:09:12 +0100

    Update 0.input.module-json.php

--- plugins/slice_json_block/pages/codes/0.input.module-json.php
+++ plugins/slice_json_block/pages/codes/0.input.module-json.php
@@ -1,4 +1,39 @@
 <fieldset>
+
+  <div class="form-group">
+    <label class="col-sm-2 control-label" for="value_1">VALUE 1:</label>
+    <div class="col-sm-10">
+      <input class="form-control" id="value_1" type="text" name="REX_INPUT_VALUE[1]" value="REX_VALUE[1]">
+      <p class="help-block">Lorem Ipsum dolor</p>
+    </div>
+  </div>
+    
+  <div class="form-group">
+    <label class="col-sm-2 control-label" for="value_2">VALUE 2:</label>
+    <div class="col-sm-10">
+      <input class="form-control" id="value_2" type="text" name="REX_INPUT_VALUE[2]" value="REX_VALUE[2]">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="col-sm-2 control-label" for="value_3">VALUE 3:</label>
+    <div class="col-sm-10 checkbox">
+      <label for="value_3">
+        <input type="checkbox" id="value_3" name="REX_INPUT_VALUE[3]" value="REX_VALUE[3]"> mehr Information
+        <p class="help-block">Lorem Ipsum dolor</p>
+      </label>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="col-sm-2 control-label">VALUE 4:</label>
+    <div class="col-sm-10">
+      <textarea cols="1" rows="6" class="form-control" id="value_4"  name="REX_INPUT_VALUE[4]">REX_VALUE[4]</textarea>
+    </div>
+  </div>
+</fieldset>
+
+<fieldset>
   <legend>JSON-Values</legend>
 
   <?php $index = 0;foreach('REX_JSON_VALUE[5]' as $key => $data) {?>
@@ -13,22 +48,42 @@
       </li>
     </ul>
 
-    <dl class="rex-form-group form-group">
-      <dt><label for="json_value_5_<?=$index;?>_title">Titel:</label></dt>
-      <dd><input class="form-control" id="json_value_5_<?=$index;?>_title" type="text" name="REX_INPUT_VALUE[5][<?=$index;?>][title]" value="<?=$data['title'];?>" ></dd>
-    </dl>
-    <dl class="rex-form-group form-group">
-      <dt><label for="json_value_5_<?=$index;?>_eins">Eins</label></dt>
-      <dd><input class="form-control" id="json_value_5_<?=$index;?>_eins" type="text" name="REX_INPUT_VALUE[5][<?=$index;?>][eins]" value="<?=$data['eins'];?>" ></dd>
-    </dl>
-    <dl class="rex-form-group form-group">
-      <dt><label for="json_value_5_<?=$index;?>_zwei">Zwei</label></dt>
-      <dd><input class="form-control" id="json_value_5_<?=$index;?>_zwei" type="text" name="REX_INPUT_VALUE[5][<?=$index;?>][zwei]" value="<?=$data['zwei'];?>" ></dd>
-    </dl>
-    <dl class="rex-form-group form-group">
-      <dt><label for="json_value_5_<?=$index;?>_drei">Drei</label></dt>
-      <dd><input class="form-control" id="json_value_5_<?=$index;?>_drei" type="text" name="REX_INPUT_VALUE[5][<?=$index;?>][drei]" value="<?=$data['drei'];?>" ></dd>
-    </dl>
+    <div class="form-group">
+      <label class="col-sm-2 control-label" for="json_value_5_<?=$index;?>_title">Titel:</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="json_value_5_<?=$index;?>_title" type="text" name="REX_INPUT_VALUE[5][<?=$index;?>][title]" value="<?=$data['title'];?>">
+      </div>
+    </div>
+
+
+
+    <div class="form-group">
+      <label class="col-sm-2 control-label" for="json_value_5_<?=$index;?>_eins">Eins:</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="json_value_5_<?=$index;?>_eins" type="text" name="REX_INPUT_VALUE[5][<?=$index;?>][eins]" value="<?=$data['eins'];?>">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-sm-2 control-label" for="json_value_5_<?=$index;?>_vier">Vier</label>
+      <div class="col-sm-10">
+        <?php 
+        $Media = new rex_input_mediabutton();
+        $Media->setButtonId(($index+1));
+        $Media->setAttribute('name','MEDIA['.($index+1).']');
+        if($Slice)
+          $Media->setValue($Slice->getMedia(($index+1)));
+        echo $Media->getHtml();
+        ?>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-sm-2 control-label" for="json_value_5_<?=$index;?>_fuenf">Fünf</label>
+      <div class="col-sm-10">
+        <textarea cols="1" rows="6" class="form-control rredactorEditor-full" id="rredactor_json_value_5_<?=$index;?>_fuenf" name="REX_INPUT_VALUE[5][<?=$index;?>][fuenf]"><?=$data['fuenf'];?></textarea>
+      </div>
+    </div>
 
     <ul class="icons">
       <li class="btn btn-add btn-move">
